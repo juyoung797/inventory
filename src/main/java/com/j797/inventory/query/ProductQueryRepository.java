321d713b1eaeb617f94ec61c400ea4f2b25b33e8
juyoung <juyoungjeong.797@gmail.com>
2025-07-21 09:48:21 +0900

    ProductQueryRepository

--- src/main/java/com/j797/inventory/query/ProductQueryRepository.java
+++ src/main/java/com/j797/inventory/query/ProductQueryRepository.java
@@ -2,6 +2,7 @@ package com.j797.inventory.query;
 
 import com.j797.inventory.model.Product;
 import com.j797.inventory.model.QProduct;
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -16,5 +17,18 @@ public class ProductQueryRepository {
 
     public List<Product> search(ProductSearchCondition condition) {
         QProduct product = QProduct.product;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (condition.getName() != null && !condition.getName().isBlank()) {
+            builder.and(product.name.containsIgnoreCase(condition.getName()));
+        }
+        if (condition.getMinPrice() != null) {
+            builder.and(product.price.goe(condition.getMinPrice()));
+        }
+        if (condition.getMaxPrice() != null) {
+            builder.and(product.price.loe(condition.getMaxPrice()));
+        }
+
+        return queryFactory.selectFrom(product).where(builder).orderBy(product.name.asc()).fetch();
     }
 }
